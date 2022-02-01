fa92aead2de2d2d78c4b5ece7c857557ff478ccb
Mustafa Gözcü <gozcummustafa@gmail.com>
2022-02-01 23:18:02 +0300

    classes homework added

---
+++ Classes/Classes/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class CustomerManager
+    {
+        public void Add()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+
+    class ProductManager
+    {
+        public void Add()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
